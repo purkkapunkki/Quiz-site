2d851957f191961f1a9af0242cf7763f7f2f036e
purkkapunkki <daftm4il@hotmail.com>
2023-05-30 01:23:26 +0300

    Fix getRandomQuestion response format

--- drill-and-practice/routes/controllers/apiController.js
+++ drill-and-practice/routes/controllers/apiController.js
@@ -6,10 +6,16 @@ const getRandomQuestion = async ({ response }) => {
   let body;
   if (question) {
     const answerOptions = await answerOptionService.getAnswerOptions(question.id);
+    let answerOptionList = [];
+    answerOptions.forEach((option) => {
+      answerOptionList.push(
+        { optionId: option.id, optionText: option.option_text }
+      );
+    });
     body = {
       "questionId": question.id,
       "questionText": question.question_text,
-      "answerOptions": answerOptions,
+      "answerOptions": answerOptionList,
     };
   } else {
     body = {};
